09acfb6839c23ae92acbcdc0ddfc68630b51c5eb
Sergey <you@easd.com>
2022-06-24 17:02:15 +0300

    bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+
 import name from '../src/cli.js'
 console.log('node bin/brain-games.js');
 console.log('Welcome to the Brain Games!');
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-function name(userName){
-	var readlineSync = require('readline-sync');
-	var userName = readlineSync.question('May I have your name? ');
-	console.log('Hi ' + userName + '!');
+function name(usNam){
+	let readlineSync = import('readline-sync');
+let userName = readlineSync.question('May I have your name? ')
+console.log('Hi ' + userName + '!')
 };
 export default name;
