fb94a1ac6519fb6df3623e281db5df742abd48ad
Sergey <you@easd.com>
2022-06-24 13:58:38 +0300

    bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
+import name from '../src/cli.js'
 console.log('node bin/brain-games.js');
 console.log('Welcome to the Brain Games!');
-import name from '../src/cli.js' 
-name(userName);
+ 
+name();
