c096565eadaaf6a2743021b675fd9fd2c8d1508f
Sergey <you@easd.com>
2022-06-24 17:44:30 +0300

    src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,5 @@
 import readlineSync from 'readline-sync';
 function name(usNam){
-	let readlineSync = import('readline-sync');
 	const userName = readlineSync.question('May I have your name? ');
 	console.log('Hi ' + userName + '!');
 };
