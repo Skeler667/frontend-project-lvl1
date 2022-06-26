1f0d0d39717a06bd3e8624f0c1ed5ddffc05a50e
kur1977 <kur1977@gmail.com>
2022-06-26 14:05:40 +0300

    .eslintrc.yml

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
-function name(usNam){
-	const userName = readlineSync.question('May I have your name? ');
-	console.log('Hi ' + userName + '!');
-};
+
+function name() {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${userName}!`);
+}
 export default name;
