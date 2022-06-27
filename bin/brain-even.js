6320a7b2cc1dd4de5b4fdbeabb378fd4b000bea0
SuperSergey <loloalilqwertykop@gmail.com>
2022-06-27 23:46:04 +0300

    fiveStep

---
+++ bin/brain-even.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import name from '../src/cli.js';
+import readlineSync from 'readline-sync';
+
+console.log('node bin/brain-games.js');
+console.log('Welcome to the Brain Games!');
+name();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+// функция нахождения рандомного числа.
+const getRandomNumb = (max) => {
+  return Math.floor(Math.random() * 10);
+};
+// вывод вопроса с рандомным числом.
+console.log(`Question: ${getRandomNumb()}`);
+// ответ на вопрос с рандомным числом.
+let variant = readlineSync.question('Your answer: ');
+// функция проверки и вывод результата рандомного числа.
+const getEven = (variant) => {
+	if (getRandomNumb % 2 == 0 && variant == 'yes') {
+		console.log('Correct!')
+	}else if (getRandomNumb % 2 !== 0 && variant == 'no') {
+		console.log('Correct!')
+	}else {
+		console.log(`'${variant}' is wrong answer ;(. Correct answer was 'no'.Let's try again,`);
+	}
+}
+getEven(variant);
