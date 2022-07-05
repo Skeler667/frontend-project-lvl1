c6f9862c99a7383040c9acef1baf0df4c001d76d
SuperSergey <loloalilqwertykop@gmail.com>
2022-07-05 21:44:37 +0300

    src/index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const gameEngine = (rules, getGameData) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hi ${userName}!`);
+  console.log(`Hello, ${userName}!`);
   console.log(rules);
 
   const roundsCount = 3;
