2561c885f13f5db856c6562220c34bfbea0e3934
SuperSergey <loloalilqwertykop@gmail.com>
2022-07-12 01:23:22 +0300

    'fixUtil'

--- src/utilites.js
+++ src/utilites.js
@@ -1,7 +1,6 @@
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-export const getArrayRandom = (array) => {
-  const rand = Math.floor(Math.random() * array.length);
-  const rValue = array[rand];
-  return rValue;
+export const getRandomIndex = (array) => {
+  const randomIndex = array[getRandomNumber(0, array.length - 1)];
+  return randomIndex;
 };
