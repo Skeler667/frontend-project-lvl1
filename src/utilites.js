66b04ad6a796a2eb00fbf7110c0a6aad336ea270
SuperSergey <loloalilqwertykop@gmail.com>
2022-07-13 02:25:43 +0300

    'addArrayRandomFunc'

--- src/utilites.js
+++ src/utilites.js
@@ -1,6 +1,4 @@
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-export const getRandomIndex = (array) => {
-  const randomIndex = array[getRandomNumber(0, array.length - 1)];
-  return randomIndex;
-};
+const getRandomIndex = array => getRandomNumber(0, array.length - 1);
+
