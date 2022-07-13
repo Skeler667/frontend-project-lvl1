61851d3483b3b3201231d2834368701ba61e7906
SuperSergey <loloalilqwertykop@gmail.com>
2022-07-13 22:04:53 +0300

    'refactoringPackageAndUtilites'

--- src/utilites.js
+++ src/utilites.js
@@ -1,4 +1,3 @@
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const getRandomIndex = array => getRandomNumber(0, array.length - 1);
-
+export const getRandomIndex = (array) => getRandomNumber(0, array.length - 1);
