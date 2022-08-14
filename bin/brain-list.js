43090b49afbe46f9191aea763f7534e71b618b7e
SuperSergey <loloalilqwertykop@gmail.com>
2022-08-14 11:49:27 +0300

    'listAdd'

--- bin/brain-list.js
+++ bin/brain-list.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
-const games = "1. Brain Even - Это игра 'Проверка на чётность'\n" + "2. Brain Calc - Это игра 'Калькулятор'\n" + "3. Brain GCD - Это игра 'По поиску наибольшего общего делителя (НОД)'\n" + "4. Brain Progression - Это игра 'Арефметическая прогрессия'\n" + "5. Brain Prime - Это игра 'Просто ли число?'";
+const games = '1. Brain Even - Это игра проверка на чётность\n2. Brain Calc - Это игра калькулятор\n3. Brain GCD - Это игра по поиску наибольшего общего делителя (НОД)\n4. Brain Progression - Это игра арефметическая прогрессия\n5. Brain Prime - Это игра просто ли число?';
 console.log(games);
